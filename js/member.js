e482c2a64491699827410c8f1caa7b874f4729b5
蒋冬 <125333110@qq.com>
2018-09-21 17:09:10 +0800

    Update member.js

--- js/member.js
+++ js/member.js
@@ -1,202 +1,202 @@
 var member = [
   {
-    "phone": "0001",
+    "phone": "总经办",
     "name": "李奎"
   },
   {
-    "phone": "0001",
+    "phone": "总经办",
     "name": "今生"
   },
   {
-    "phone": "0001",
+    "phone": "人事行政",
     "name": "李宁"
   },
   {
-    "phone": "0001",
+    "phone": "人事行政",
     "name": "郭玲"
   },
   {
-    "phone": "0001",
+    "phone": "人事行政",
     "name": "何玲"
   },
   {
-    "phone": "0001",
+    "phone": "财务部",
     "name": "张华"
   },
   {
-    "phone": "0001",
+    "phone": "线上运营部",
     "name": "杨斌"
   },
   {
-    "phone": "0001",
+    "phone": "线上运营部",
     "name": "夏永明"
   },
   {
-    "phone": "0001",
+    "phone": "线上运营部",
     "name": "周坤龙"
   },
   {
-    "phone": "0001",
+    "phone": "线上运营部",
     "name": "黎甜"
   },
   {
-    "phone": "0001",
+    "phone": "线下运营部",
     "name": "王坤林"
   },
   {
-    "phone": "0001",
+    "phone": "线下运营部",
     "name": "庞静"
   },
   {
-    "phone": "0001",
+    "phone": "线下运营部",
     "name": "朱兵"
   },
   {
-    "phone": "0001",
+    "phone": "线下运营部",
     "name": "彭棕佺"
   },
   {
-    "phone": "0001",
+    "phone": "线下运营部",
     "name": "王迷"
   },
   {
-    "phone": "0001",
+    "phone": "线下运营部",
     "name": "李兰"
   },
   {
-    "phone": "0001",
+    "phone": "线下运营部",
     "name": "赵莉"
   },
   {
-    "phone": "0001",
+    "phone": "线下运营部",
     "name": "岳倩"
   },
   {
-    "phone": "0001",
+    "phone": "线下运营部",
     "name": "王玲"
   },
   {
-    "phone": "0001",
+    "phone": "线下运营部",
     "name": "刘建华"
   },
   {
-    "phone": "0001",
+    "phone": "线下运营部",
     "name": "宋道灵"
   },
   {
-    "phone": "0001",
+    "phone": "线下运营部",
     "name": "尹力"
   },
   {
-    "phone": "0001",
+    "phone": "设计部",
     "name": "杜亮"
   },
   {
-    "phone": "0001",
+    "phone": "设计部",
     "name": "衡蕾"
   },
   {
-    "phone": "0001",
+    "phone": "设计部",
     "name": "刘鹏飞"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "曾涛"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "蒋冬"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "胡剑"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "孙东"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "卫思思"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "廖欢"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "杨诗雨"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "陈飞宇"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "严智超"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "粟涛"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "孟菲"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "廖航"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "何佳骏"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "蒋婧娴"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "李扶远"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "钟文鹏"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "何友志"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "魏成辉"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "黄雯熙"
   },
   {
-    "phone": "0001",
+    "phone": "开发部",
     "name": "杨睿"
   },
   {
-    "phone": "0001",
+    "phone": "工程部",
     "name": "魏露"
   },
   {
-    "phone": "0001",
+    "phone": "工程部",
     "name": "章顺宽"
   },
   {
-    "phone": "0001",
+    "phone": "工程部",
     "name": "金潇"
   },
   {
-    "phone": "0001",
+    "phone": "工程部",
     "name": "景燕琼"
   },
   {
-    "phone": "0001",
+    "phone": "工程部",
     "name": "向媛媛"
   }
 ];
